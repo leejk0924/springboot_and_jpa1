5567c4667acac9d2067b9f7f0ba4bee003e73187
Jung-Kwon Lee <akdirdl@gmail.com>
2022-06-20 18:01:59 +0900

    lecture: 상품 도메인 개발 (상품 리포지토리 개발)

---
+++ jpashop/src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -0,0 +1,33 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.item.Item;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+    private final EntityManager em;
+
+
+    // item은 jdbc에 저장하기 전까지 id값이 없다. id 값이 없다는건 신규 등록 해야한다는 얘기 (getId가 있다는건 기존의 db에 데이터가 있다는 이야기)
+    public void save(Item item) {
+        if (item.getId() == null) {
+            em.persist(item);
+        } else {
+            em.merge(item);
+        }
+    }
+
+    public Item findOne(Long id) {
+        return em.find(Item.class, id);
+    }
+
+    public List<Item> findAll() {
+        return em.createQuery("select i from Item i", Item.class)
+                .getResultList();
+    }
+}
