6981e7ab5e5336fa9b26f4079069d6f9a6260160
Jung-Kwon Lee <akdirdl@gmail.com>
2022-06-21 23:11:38 +0900

    lecture: 웹 계층 개발(홈 화면과 레이아웃)

---
+++ jpashop/src/main/java/jpabook/jpashop/controller/HomeController.java
@@ -0,0 +1,17 @@
+package jpabook.jpashop.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Slf4j
+public class HomeController{
+//    Logger log = LoggerFactory.getLogger(getClass()); // 해당 코드의 롬복이 @Slf4j
+
+    @RequestMapping("/")
+    public String home() {
+        log.info("home controller");
+        return "home";
+    }
+}
